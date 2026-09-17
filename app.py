98e1a0181e5eb5a2671f7dee898cbd0b0a170998
mithun-ksms <mithunsurriyaks@gmail.com>
2026-09-17 14:08:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def get_cv_skills(cv_text, client):
 
     # send to Gemini using new SDK
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3.6-flash",
         contents=prompt
     )
 
